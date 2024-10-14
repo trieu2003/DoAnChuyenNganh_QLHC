af7043aa76db1732d4ca61ec1d3ddb84d87e2428
Trieu ne <huynhthanhtrieu00@gmail.com>
2024-10-15 02:39:04 +0700

    tuc qua ma

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Sidebar from './components/Sidebar';
-import Home from './pages/Home';
-import ChemicalManagement from './pages/ChemicalManagement';
-import PurchaseRequest from './pages/PurchaseRequest';
-import InventoryManagement from './pages/InventoryManagement';
-import ChemicalForecasting from './pages/ChemicalForecasting';
-import ChemicalUsageManagement from './pages/ChemicalUsageManagement';
-import ExpiredChemicalManagement from './pages/ExpiredChemicalManagement';
-import Reporting from './pages/Reporting';
-import Settings from './pages/Settings';
-import { Container } from '@mui/material';
-import Login from './Login';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import ChemicalManagement from "./pages/ChemicalManagement";
+import PurchaseRequest from "./pages/PurchaseRequest";
+import InventoryManagement from "./pages/InventoryManagement";
+import ChemicalForecasting from "./pages/ChemicalForecasting";
+import ChemicalUsageManagement from "./pages/ChemicalUsageManagement";
+import ExpiredChemicalManagement from "./pages/ExpiredChemicalManagement";
+import Reporting from "./pages/Reporting";
+import Settings from "./pages/Settings";
+import { Container } from "@mui/material";
+import Login from "./Login";
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -29,6 +29,7 @@ function App() {
   return (
     <Router>
       <div>
+        <h1>tức quá rồi </h1>
         <Header onOpenSidebar={handleSidebarOpen} />
         <Sidebar open={sidebarOpen} onClose={handleSidebarClose} />
         <Container>
@@ -36,12 +37,27 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/Login" element={<Login />} />
 
-            <Route path="/chemical-management" element={<ChemicalManagement />} />
+            <Route
+              path="/chemical-management"
+              element={<ChemicalManagement />}
+            />
             <Route path="/purchase-request" element={<PurchaseRequest />} />
-            <Route path="/inventory-management" element={<InventoryManagement />} />
-            <Route path="/chemical-forecasting" element={<ChemicalForecasting />} />
-            <Route path="/chemical-usage" element={<ChemicalUsageManagement />} />
-            <Route path="/expired-chemicals" element={<ExpiredChemicalManagement />} />
+            <Route
+              path="/inventory-management"
+              element={<InventoryManagement />}
+            />
+            <Route
+              path="/chemical-forecasting"
+              element={<ChemicalForecasting />}
+            />
+            <Route
+              path="/chemical-usage"
+              element={<ChemicalUsageManagement />}
+            />
+            <Route
+              path="/expired-chemicals"
+              element={<ExpiredChemicalManagement />}
+            />
             <Route path="/reporting" element={<Reporting />} />
             <Route path="/settings" element={<Settings />} />
           </Routes>
